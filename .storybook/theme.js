85b0ab4a8ba90e5acab3af23ccd64d54ed4066ba
Alexander Schwartzberg <aeksco@gmail.com>
2020-12-19 19:51:36 -0500

    Updated theme to be more consistent with Tailwind styles

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -2,9 +2,37 @@ import { create } from "@storybook/theming/create";
 // import LogoSvg from "./assets/logo.svg";
 
 export default create({
-  // base: "dark",
-  base: "light",
-  brandTitle: "React OpenJSCAD",
-  brandUrl: "https://github.com/aeksco/openjscad-react"
-  // brandImage: LogoSvg
+    base: "light", // Or "dark"
+    brandTitle: "React OpenJSCAD",
+    brandUrl: "https://github.com/aeksco/openjscad-react",
+    // brandImage: LogoSvg
+    // brandImage: "https://placehold.it/350x150",
+
+    // colorPrimary: "hotpink",
+    // colorSecondary: "deepskyblue",
+
+    // UI
+    appBg: "#f5f6f9",
+    appContentBg: "#f5f6f9",
+    // appBorderColor: "grey",
+    // appBorderRadius: 4,
+
+    // Typography
+    // fontBase: '"Open Sans", sans-serif',
+    // fontCode: "monospace",
+
+    // Text colors
+    // textColor: "black",
+    // textInverseColor: "rgba(255,255,255,0.9)",
+
+    // Toolbar default and active colors
+    // barTextColor: "silver",
+    // barSelectedColor: "black",
+    // barBg: "hotpink",
+
+    // Form colors
+    // inputBg: "white",
+    // inputBorder: "silver",
+    // inputTextColor: "black",
+    // inputBorderRadius: 4,
 });
